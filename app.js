9ec0b4aa04d8918c136553bd976332d65efe0abd
D <d@diki.dev>
2020-08-13 15:48:53 +0100

    Beers Trigger Random on Click
    Event listeners added for clicks, adding volume and unit to title bar.

--- app.js
+++ app.js
@@ -2,8 +2,16 @@
 
 document.addEventListener('DOMContentLoaded', () => {
     const startBtn = document.querySelector('.beer-button')
-    
+    const randomBeer = document.querySelector('.random-beer')
+    const descriptionDisplay = document.querySelector('.description')
+
+
+
 // Fetch method - fetch JSON file & print to console, http response
+function getData(e) {
+    e.preventDefault()
+
+
     fetch('https://api.punkapi.com/v2/beers/random')
         .then(response => {
             return response.json();
@@ -17,7 +25,15 @@ document.addEventListener('DOMContentLoaded', () => {
             const {volume} = data[0]
             const volumeValue = volume.value
             const volumeUnit = volume.unit
+
+            randomBeer.innerHTML = name + ' ' + volumeValue + volumeUnit
+            descriptionDisplay.innerHTML = description
+            volumeDisplay.innerHTML = volumeValue + volumeUnit
         
         })
+}    
+
+startBtn.addEventListener('click', getData)
+
 })
 
